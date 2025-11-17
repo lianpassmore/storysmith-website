765b9a63c8d0bcdfe9540b0023aa3123c17470ae
lianpassmore <lianpassmore@gmail.com>
2025-11-18 09:26:35 +1300

    "widg fix"

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_B3m0CZ9Q.mjs';
-import { manifest } from './manifest_B1TR0t3J.mjs';
+import { manifest } from './manifest_CLWcURS_.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -31,7 +31,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "c362fb60-ffe0-4ad9-94a0-c52fbe0d6d4c",
+    "middlewareSecret": "1475e633-924d-4399-a719-528f7a97b4bc",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
